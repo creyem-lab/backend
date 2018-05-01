3f32a964501b0c1cbc603e59050f91111ae1a854
Colin Campbell <colincampbell321123@hotmail.com>
2018-05-01 22:57:53 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --chdir creyem_lab_api index:app
---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export FLASK_APP=./creyem_lab_api/index.py
+export FLASK_ENV=development
+source $(pipenv --venv)/bin/activate
+flask run -h 0.0.0.0
