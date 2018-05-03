1d78b7143a2fdbb27807315509d4e41fac19ea81
Colin Campbell <colincampbell321123@hotmail.com>
2018-05-03 13:01:03 +0100

    Change hotspot tooltip fields

--- creyem_lab_api/model/hotspot.py
+++ creyem_lab_api/model/hotspot.py
@@ -2,13 +2,13 @@ from marshmallow import Schema, fields
 
 
 class Hotspot():
-    def __init__(self, _id, case_id, pitch, yaw, cssClass, createTooltipArgs):
+    def __init__(self, _id, case_id, pitch, yaw, hotspotType, text):
         self._id = _id
         self.case_id = case_id
         self.pitch = pitch
         self.yaw = yaw
-        self.cssClass = cssClass
-        self.createTooltipArgs = createTooltipArgs
+        self.hotspotType = hotspotType
+        self.text = text
 
     def __repr__(self):
         return '<Hotspot(name={self.id!r})>'.format(self=self)
@@ -19,5 +19,5 @@ class HotspotSchema(Schema):
     case_id = fields.Str()
     pitch = fields.Number()
     yaw = fields.Number()
-    cssClass = fields.Str()
-    createTooltipArgs = fields.Str()
+    hotspotType = fields.Str()
+    text = fields.Str()
